a55e56264e5a9b73d7d12202de27ab8a64e46145
Brian McCoy <sendthistobrian@gmail.com>
2015-07-08 19:08:55 -0500

    editted home jumbotron

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
-         before_create :generate_default_profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+has_one :profilepicture
 
-    def generate_default_profile
-    	self.profile = Profile.new
-    end
 end
